5e708bd216ab1f96826f2fa029d8c9537b72b1c7
Mitchel Wassler <mitchelwassler@Mitchels-MacBook-Pro-2.local>
2019-04-15 22:03:59 -0400

    finished basic api functionality

--- app/controllers/v1/runs_controller.rb
+++ app/controllers/v1/runs_controller.rb
@@ -8,32 +8,35 @@ class V1::RunsController < ApplicationController
   end
 
   def create
+    feature = RGeo::GeoJSON.decode(params.to_json)
+    run = Run.new
+    run.path = feature.geometry.as_text
+    run.title = feature['title']
+    run.duration = feature['duration']
+    run.distance = feature['distance']
+    if run.save
+      render :json => run, :status => 201
+    end
   end
 
   def update
+    run = Run.find params[:id]
+    run.title = params[:title]
+    if run.save
+      render :json => run, :status => 200
+    end
   end
 
   def destroy
+    Run.destroy params[:id]
+    render :json => {}, :status => 200
   end
 
   def matched
     render json: Run.matched(params[:run_id])
   end
 
-  private
 
-    def run_params
-      params.permit(
-        geometry: [
-          :coordinates
-        ],
-        properties: [
-          :duration,
-          :distance,
-          :title
-        ]
-      )
-
-    end
 
 end
+
--- spec/requests/runs_spec.rb
+++ spec/requests/runs_spec.rb
@@ -40,7 +40,81 @@ RSpec.describe "Runs", type: :request do
 
       expect(json["run"].to_json).to eq(geojson)
     end
+  end
 
+  describe "POST /api/v1/runs" do
+    it "can save a geoJSON run" do
+      geojson = {
+        "type" => "Feature",
+        "geometry" => {
+          "type" => "LineString",
+          "coordinates" => [
+            [-122.48369693756104, 37.83381888486939],
+            [-122.48348236083984, 37.83317489144141],
+            [-122.48339653015138, 37.83270036637107],
+            [-122.48356819152832, 37.832056363179625]
+          ]
+      },
+        "properties" => {
+            "duration" => 5000,
+            "distance" => 0.5,
+            "title" => "A test run",
+        }
+      }
+
+      headers = { "CONTENT_TYPE" => "application/json" }
+
+      post "/api/v1/runs", :params => geojson.to_json, :headers => headers
+
+      expect(response).to have_http_status(201)
+
+      get '/api/v1/runs'
+
+      expect(response).to have_http_status(200)
+
+      json = JSON.parse(response.body)
+
+      expect(json['runs'].count).to eq(1)
+    end
+  end
+
+  describe "PUT /api/v1/runs/{:id}" do
+    it "can update the title of a run" do
+       run = create(:run)
+
+       headers = { "CONTENT_TYPE" => "application/json" }
+
+       properites = {
+          :title => "Updated run",
+       }
+       put "/api/v1/runs/#{run.id}", :params => properites.to_json, :headers => headers
+
+       expect(response).to have_http_status(200)
+
+       get "/api/v1/runs/#{run.id}"
+
+       json = JSON.parse(response.body)
+
+       expect(json['run']['properties']['title']).to eq("Updated run")
+
+
+    end
+  end
+
+  describe "DELETE /api/v1/runs/{:id}" do
+    it "can delete a run" do
+      run_a = create(:run)
+
+      delete "/api/v1/runs/#{run_a.id}"
+
+      expect(response).to have_http_status(200)
+
+      get "/api/v1/runs"
+
+      json = JSON.parse(response.body)
+
+      expect(json['runs'].count).to eq(0)
+    end
   end
 
   describe "GET /api/v1/runs/{:id}/matched" do
